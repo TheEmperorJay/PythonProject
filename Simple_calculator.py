f7e02f01532a34621eec892777e862758a833172
TheEmperorJay <meshram.jay1525@gmail.com>
2025-04-17 17:18:24 +0530

    New

---
+++ Simple_calculator.py
@@ -0,0 +1,10 @@
+First_number = int(input('First number: '))
+Second_number = int(input('Second number: '))
+Addition = First_number + Second_number
+Subtraction = First_number - Second_number
+Multiplication = First_number * Second_number
+Division = First_number / Second_number
+print('Addition:', Addition)
+print('Subtraction:', Subtraction)
+print('Multiplication:', Multiplication)
+print('Division:', Division)
---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+First_number = int(input('First number: '))
+Second_number = int(input('Second number: '))
+Addition = First_number + Second_number
+Subtraction = First_number - Second_number
+Multiplication = First_number * Second_number
+Division = First_number / Second_number
+print('Addition:', Addition)
+print('Subtraction:', Subtraction)
+print('Multiplication:', Multiplication)
+print('Division:', Division)
